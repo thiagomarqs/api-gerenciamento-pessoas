3280aa0dfcd8a148d9f49f0a54d76a77d110d322
thiagomarqs <thiagodsmarques@hotmail.com>
2024-04-25 18:41:11 -0300

    project basics

---
+++ src/test/java/com/github/thiagomarqs/gerenciamentopessoas/GerenciamentopessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thiagomarqs.gerenciamentopessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentopessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
